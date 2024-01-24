cda172e7fcf606f7515f4246f8a38f45f7882c06
Venorrak <112645452+Venorrak@users.noreply.github.com>
2024-01-24 14:27:03 -0400

    change wave button on test_reverb

--- tests/test_reverb/test_reverb.ino
+++ tests/test_reverb/test_reverb.ino
@@ -12,6 +12,10 @@ float pitchknob;
 float pitchknob2;
 float pitchknob3;
 float pitchknob4;
+enum Wave_State {SIN, TRI, SAW, SQUARE};
+Wave_State wave_state = TRI;
+bool last_button_state = false;
+unsigned long prev_millis = 0;
 
 ReverbSc verb;
 
@@ -38,6 +42,8 @@ void setup() {
   num_channels = hw.num_channels;
   sample_rate = DAISY.get_samplerate();
 
+  pinMode(0, INPUT_PULLUP);
+
   osc.Init(sample_rate);
   osc.SetFreq(440);
   osc.SetAmp(0.5);
@@ -56,6 +62,36 @@ void setup() {
 }
 
 void loop() { 
+  unsigned long cur_millis = millis();
+  if (cur_millis - prev_millis > 50) {
+    prev_millis = cur_millis;
+    int current_button_state = digitalRead(0);
+    if (current_button_state == LOW && last_button_state == HIGH){
+      switch (wave_state)
+      {
+      case SIN:
+        wave_state = TRI;
+        osc.SetWaveform(osc.WAVE_TRI);
+        osc2.SetWaveform(osc.WAVE_TRI);
+        break;
+      case TRI:
+        wave_state = SAW;
+        osc.SetWaveform(osc.WAVE_SAW);
+        osc2.SetWaveform(osc.WAVE_SAW);
+        break;
+      case SAW:
+        wave_state = SQUARE;
+        osc.SetWaveform(osc.WAVE_SQUARE);
+        osc2.SetWaveform(osc.WAVE_SQUARE);
+        break;
+      case SQUARE:
+        wave_state = SIN;
+        osc.SetWaveform(osc.WAVE_SIN);
+        osc2.SetWaveform(osc.WAVE_SIN);
+      }
+    }
+    last_button_state = current_button_state;
+  }
   pitchknob = 24.0 + ((analogRead(A0) / 1023.0) * 60.0);
   pitchknob2 = 24.0 + ((analogRead(A1) / 1023.0) * 60.0);
   pitchknob3 = (analogRead(A2) / 1023.0) * 18000.0f;
