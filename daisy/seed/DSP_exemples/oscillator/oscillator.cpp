4018b56bbb513ac15c147d7a71f63640e793fb79
Kyavn <manonbro0105@gmail.com>
2024-03-04 17:22:57 -0500

    Add a map (convertValue) function and finalize the envelope module for an oscillator

--- daisy/seed/DSP_exemples/oscillator/oscillator.cpp
+++ daisy/seed/DSP_exemples/oscillator/oscillator.cpp
@@ -1,5 +1,6 @@
 #include "daisysp.h"
 #include "daisy_seed.h"
+#include <cmath>
 
 #define LEFT (i)
 #define RIGHT (i + 1)
@@ -13,6 +14,29 @@ static Adsr env;
 static Metro tick;
 static Switch3 toggleWaveForm;
 bool gate;
+
+float convertValue(int value, float new_min, float new_max) {
+    int min_value = 0;
+    int max_value = 65534;
+    
+    // Calculer la valeur normalisée entre 0 et 1
+    float normalized_value = static_cast<float>(value - min_value) / (max_value - min_value);
+    
+    // Appliquer la fonction de mise à l'échelle exponentielle
+    float scaled_value = ((new_max - new_min) * pow(normalized_value, 2)) + new_min;
+    
+    return scaled_value;
+}
+
+void changeWaveForm() 
+{
+    switch(toggleWaveForm.Read())
+    {
+        case Switch3::POS_UP: osc.SetWaveform(osc.WAVE_SQUARE); break;
+        case Switch3::POS_CENTER: osc.SetWaveform(osc.WAVE_SAW); break;
+        case Switch3::POS_DOWN: osc.SetWaveform(osc.WAVE_TRI); break;
+    }
+}
 
 static void AudioCallback(AudioHandle::InterleavingInputBuffer  in,
                           AudioHandle::InterleavingOutputBuffer out,
@@ -28,24 +52,23 @@ static void AudioCallback(AudioHandle::InterleavingInputBuffer  in,
 
         float knobFrequence = hw.adc.GetFloat(1);
         float knobAmplitude = hw.adc.GetFloat(0);
+        float knobTickFrequency = hw.adc.Get(2);
+        float knobAttack = hw.adc.Get(3);
+        float knobDecay = hw.adc.Get(4);
+        float knobSustain = hw.adc.Get(5);
         
         env_out = env.Process(gate);
+        osc_out = osc.Process();
+
         osc.SetFreq(24.0 + (fclamp(knobFrequence, 0.0f, 1.0f) * 60) );
         osc.SetAmp(env_out * knobAmplitude);
-        osc_out = osc.Process();
+        tick.SetFreq(convertValue(knobTickFrequency, 1.0f, 5.0f));
+        env.SetTime(ADSR_SEG_ATTACK, convertValue(knobAttack, 1.0f, 5.0f));
+        env.SetTime(ADSR_SEG_DECAY, convertValue(knobDecay, 1.0f, 5.0f));
+        env.SetSustainLevel(convertValue(knobSustain, 0.0f, 1.0f));
 
         out[LEFT]  = osc_out;
         out[RIGHT] = osc_out;
-    }
-}
-
-void changeWaveForm() 
-{
-    switch(toggleWaveForm.Read())
-    {
-        case Switch3::POS_UP: osc.SetWaveform(osc.WAVE_SQUARE); break;
-        case Switch3::POS_CENTER: osc.SetWaveform(osc.WAVE_SAW); break;
-        case Switch3::POS_DOWN: osc.SetWaveform(osc.WAVE_TRI); break;
     }
 }
 
@@ -58,15 +81,16 @@ int main(void)
 
     toggleWaveForm.Init(seed::D29, seed::D30);
     
-    AdcChannelConfig adcConfig[5];
+    AdcChannelConfig adcConfig[6];
 
     adcConfig[0].InitSingle(hw.GetPin(20));
     adcConfig[1].InitSingle(hw.GetPin(21));
     adcConfig[2].InitSingle(hw.GetPin(19));
     adcConfig[3].InitSingle(hw.GetPin(18));
     adcConfig[4].InitSingle(hw.GetPin(17));
+    adcConfig[5].InitSingle(hw.GetPin(16));
    
-    hw.adc.Init(adcConfig, 5);
+    hw.adc.Init(adcConfig, 6);
     hw.adc.Start();
     hw.StartLog();
 
@@ -76,11 +100,11 @@ int main(void)
     env.Init(sample_rate);
 
     // Set up metro to pulse x second
-    tick.Init(1.2f, sample_rate);
+    tick.Init(1.0f, sample_rate);
 
     //Set envelope parameters
-    env.SetTime(ADSR_SEG_ATTACK, 1.2f);
-    env.SetTime(ADSR_SEG_DECAY, 1.2f);
+    env.SetTime(ADSR_SEG_ATTACK, 1.0f);
+    env.SetTime(ADSR_SEG_DECAY, 1.0f);
 
     env.SetSustainLevel(.50);
 
@@ -95,6 +119,6 @@ int main(void)
     while(1) 
     {
         changeWaveForm();
-        hw.PrintLine("Input 0: %d", (hw.adc.Get(0)));
+        hw.PrintLine("Input 1: %d", hw.adc.Get(5));      
     }
 }
