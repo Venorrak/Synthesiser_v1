938cda08935dc28ac44a71c1a492bdea7b6984b7
Venorrak <112645452+Venorrak@users.noreply.github.com>
2024-03-17 17:05:41 -0400

    Tried a different method of receiving midi signal from arduino

--- daisy/seed/MidiTest/MidiTest.cpp
+++ daisy/seed/MidiTest/MidiTest.cpp
@@ -1,101 +1,115 @@
-#include "daisysp.h"
+/** Example of setting reading MIDI Input via UART
+ *  
+ * 
+ *  This can be used with any 5-pin DIN or TRS connector that has been wired up
+ *  to one of the UART Rx pins on Daisy.
+ *  This will use D14 as the UART 1 Rx pin
+ * 
+ *  This example will also log incoming messages to the serial port for general MIDI troubleshooting
+ */
 #include "daisy_seed.h"
-#include "hid/midi.h"
-#include <string>
 
-
+/** This prevents us from having to type "daisy::" in front of a lot of things. */
 using namespace daisy;
-using namespace daisysp;
 
-
-DaisySeed hw;
-Oscillator osc;
-Svf        filt;
-MidiHandler midi;
-
-void AudioCallback(AudioHandle::InputBuffer  in,
-                   AudioHandle::OutputBuffer out,
-                   size_t                    size)
+/** Fills string with string representation of MidiEvent::Type
+ *  str needs to be at least 16 bytes long to store the data
+ * TODO: Move this into MIDI lib or something 
+*/
+void GetMidiTypeAsString(MidiEvent& msg, char* str)
 {
-    float sig;
-    for(size_t i = 0; i < size; i++)
+    switch(msg.type)
     {
-        sig = osc.Process();
-        filt.Process(sig);
-        for(size_t chn = 0; chn < 4; chn++)
-        {
-            out[chn][i] = filt.Low();
-        }
+        case NoteOff: strcpy(str, "NoteOff"); break;
+        case NoteOn: strcpy(str, "NoteOn"); break;
+        case PolyphonicKeyPressure: strcpy(str, "PolyKeyPres."); break;
+        case ControlChange: strcpy(str, "CC"); break;
+        case ProgramChange: strcpy(str, "Prog. Change"); break;
+        case ChannelPressure: strcpy(str, "Chn. Pressure"); break;
+        case PitchBend: strcpy(str, "PitchBend"); break;
+        case SystemCommon: strcpy(str, "Sys. Common"); break;
+        case SystemRealTime: strcpy(str, "Sys. Realtime"); break;
+        case ChannelMode: strcpy(str, "Chn. Mode"); break;
+        default: strcpy(str, "Unknown"); break;
     }
 }
 
-// Typical Switch case for Message Type.
-void HandleMidiMessage(MidiEvent m)
+/** Global Hardware access */
+DaisySeed       hw;
+MidiUartHandler midi;
+
+/** FIFO to hold messages as we're ready to print them */
+FIFO<MidiEvent, 128> event_log;
+
+int main(void)
 {
-    switch(m.type)
+    /** Initialize our hardware */
+    hw.Init();
+
+    hw.StartLog(true);
+
+    MidiUartHandler::Config midi_config;
+    midi.Init(midi_config);
+
+    midi.StartReceive();
+
+    uint32_t now      = System::GetNow();
+    uint32_t log_time = System::GetNow();
+
+    /** Infinite Loop */
+    while(1)
     {
-        case NoteOn:
+        now = System::GetNow();
+
+        /** Process MIDI in the background */
+        midi.Listen();
+
+        /** Loop through any MIDI Events */
+        while(midi.HasEvents())
         {
-            NoteOnEvent p = m.AsNoteOn();
-            // This is to avoid Max/MSP Note outs for now..
-            if(m.data[1] != 0)
+            MidiEvent msg = midi.PopEvent();
+
+            /** Handle messages as they come in 
+             *  See DaisyExamples for some examples of this
+             */
+            switch(msg.type)
             {
-                p = m.AsNoteOn();
-                osc.SetFreq(mtof(p.note));
-                osc.SetAmp((p.velocity / 127.0f));
-            }
-        }
-        break;
-        case ControlChange:
-        {
-            ControlChangeEvent p = m.AsControlChange();
-            switch(p.control_number)
-            {
-                case 1:
-                    // CC 1 for cutoff.
-                    filt.SetFreq(mtof((float)p.value));
-                    break;
-                case 2:
-                    // CC 2 for res.
-                    filt.SetRes(((float)p.value / 127.0f));
+                case NoteOn:
+                    // Do something on Note On events
+                    {
+                        uint8_t bytes[3] = {0x90, 0x00, 0x00};
+                        bytes[1] = msg.data[0];
+                        bytes[2] = msg.data[1];
+                        midi.SendMessage(bytes, 3);
+                    }
                     break;
                 default: break;
             }
-            break;
+
+            /** Regardless of message, let's add the message data to our queue to output */
+            event_log.PushBack(msg);
         }
-        default: break;
-    }
-}
 
-
-// Main -- Init, and Midi Handling
-int main(void)
-{
-    // Init
-    float samplerate;
-    hw.Init();
-    samplerate = hw.AudioSampleRate();
-
-    // Synthesis
-    osc.Init(samplerate);
-    osc.SetWaveform(Oscillator::WAVE_POLYBLEP_SAW);
-    filt.Init(samplerate);
-
-    //display
-    std::string str  = "Midi";
-    char*       cstr = &str[0];
-
-    // Start stuff.
-    hw.midi.StartReceive();
-    hw.InitSingle(15);
-    hw.StartAudio(AudioCallback);
-    for(;;)
-    {
-        hw.midi.Listen();
-        // Handle MIDI Events
-        while(hw.midi.HasEvents())
+        /** Now separately, every 5ms we'll print the top message in our queue if there is one */
+        if(now - log_time > 5)
         {
-            HandleMidiMessage(hw.midi.PopEvent());
+            log_time = now;
+            if(!event_log.IsEmpty())
+            {
+                auto msg = event_log.PopFront();
+                char outstr[128];
+                char type_str[16];
+                GetMidiTypeAsString(msg, type_str);
+                sprintf(outstr,
+                        "time:\t%ld\ttype: %s\tChannel:  %d\tData MSB: "
+                        "%d\tData LSB: %d\n",
+                        now,
+                        type_str,
+                        msg.channel,
+                        msg.data[0],
+                        msg.data[1]);
+                hw.PrintLine(outstr);
+            }
         }
     }
 }
